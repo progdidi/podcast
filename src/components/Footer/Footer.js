eb664c6806b59c5cafbebabc516679ba394689b7
Diana Sosnovskaya <progdidi@gmail.com>
2025-02-16 19:50:27 +0300

    feat: feat Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,14 +17,48 @@ const Footer = () => {
             <div className="container">
                 <div className="footer-inner">
                     <div className="footer__info">
+                        <img src={logo} alt="" className="footer__logo" />
+                        <p className="footer__info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         <SocialList/>
                     </div>
-                    <ul className="footer-menu"></ul>
-                    <ul className="footer-menu"></ul>
-                    <div className="footer__links"></div>
+
+                    <ul className="footer-menu">
+                        <li className="footer-menu__item"><a href="" className="footer-menu__link">About</a></li>
+                        <li className="footer-menu__item"><a href="" className="footer-menu__link">Testimonials</a></li>
+                        <li className="footer-menu__item"><a href="" className="footer-menu__link">Features</a></li>
+                    </ul>
+
+                    <ul className="footer-menu">
+                        <li className="footer-menu__item"><a href="" className="footer-menu__link">Episodes</a></li>
+                        <li className="footer-menu__item"><a href="" className="footer-menu__link">Pricing</a></li>
+                        <li className="footer-menu__item"><a href="" className="footer-menu__link">Blog</a></li>
+                    </ul>
+
+                    <div className="footer__links">
+                        <p className="footer__links-text">Listen to episodes on your fav platform:</p>
+                        <div className="footer__links-list platform">
+                            <a href="" className="footer__link"><img src={google} alt="" className="footer__link-img" /></a>
+                            <a href="" className="footer__link"><img src={spotify} alt="" className="footer__link-img" /></a>
+                            <a href="" className="footer__link"><img src={youtube} alt="" className="footer__link-img" /></a>
+                        </div>
+
+                        <p className="footer__links-text">App available on:</p>
+                        <div className="footer__links-list">
+                            <a href="" className="footer__link"><img src={shop1} alt="" className="footer__link-img" /></a>
+                            <a href="" className="footer__link"><img src={shop2} alt="" className="footer__link-img" /></a>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="footer-bottom"></div>
+                <div className="footer-bottom">
+                    <p className="footer-bottom__text">©2022. All Rights Reserved. Pod of Cast</p>
+
+                    <div className="footer-bottom__links">
+                        <a href="" className="footer-bottom__link">Terms</a>
+                        <a href="" className="footer-bottom__link">Privacy</a>
+                    </div>
+
+                </div>
             </div>
         </footer>
      );
