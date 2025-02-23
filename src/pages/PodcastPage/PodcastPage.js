6e5e3d9d008535a2982d0b4f1851335708c702ba
Diana Sosnovskaya <progdidi@gmail.com>
2025-02-23 12:44:58 +0300

    feat: feat PodcastPage

--- src/pages/PodcastPage/PodcastPage.js
+++ src/pages/PodcastPage/PodcastPage.js
@@ -2,6 +2,8 @@ import './podcastPage.scss';
 
 //images
 import example from './example.png';
+import host from './host.png';
+import play from './images/play.svg';
 
 //components
 import PodcastCard from '../../components/PodcastCard/PodcastCard';
@@ -20,19 +22,21 @@ const PodcastPage = () => {
                             <p className="podcast-info__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
                             <div className="podcast-info__detail">
                                 <div className="podcast-info__host">
-                                Hosted by: Jane Doe
+                                    <img src={host} alt="" className="podcast-info__host-img" />
+                                    <p className="podcast-info__host-text">Hosted by: <a href="" className="podcast-info__host-link">Jane Doe</a></p>
                                 </div>
 
                                 <p className="podcast-info__date">Sep 22, 2021</p>
                             </div>
                             <div className="podcast-info__buttons">
                                 <button className="podcast-info__btn black-btn">SUBSCRIBE</button>
-                                <button className="podcast-info__btn">LISTEN NOW (46 min)</button>
+                                <button className="podcast-info__btn listen-btn"> <img src={play} alt="" className="listen-btn__img" /> LISTEN NOW (46 min)</button>
                             </div>
                         </div>
                     </div>
 
-                    <div className="podcast__tags">Tags:
+                    <div className="podcast__tags">
+                        <p className="podcast__tags-text">Tags:</p>
                         <p className="podcast__tag tag">mind-behaviour</p>
                         <p className="podcast__tag tag">health</p>
                     </div>
@@ -45,7 +49,7 @@ const PodcastPage = () => {
                     <h2 className="latest__title section-title">Latest Episode</h2>
 
                     <div className="latest__tabs">
-                        <button className="latest__tabs-btn"> All</button>
+                        <button className="latest__tabs-btn active"> All</button>
                         <button className="latest__tabs-btn">Business</button>
                         <button className="latest__tabs-btn">Comedy</button>
                         <button className="latest__tabs-btn">Education</button>
@@ -53,7 +57,7 @@ const PodcastPage = () => {
                         <button className="latest__tabs-btn">News</button>
                         <button className="latest__tabs-btn">Tech</button>
                     </div>
-                    
+
                     <div className="latest__inner">
                         <PodcastCard/>
                         <PodcastCard/>
